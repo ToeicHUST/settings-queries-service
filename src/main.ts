efbae2704adf521c5b22a55bb853975fe8319f61
VuVanNghia <vuvannghia.work@gmail.com>
2026-02-16 18:18:54 +0700

    feat: update project configuration and enhance testing setup with MongoDB container

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { logAppBootstrap, setupSwagger } from '@toeichust/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix(`api`);
+
+  const configService = app.get(ConfigService);
+
+  setupSwagger(app);
+
+  const port =
+    configService.get<number>('MICROSERVICES_SETTINGS_QUERIES_SERVICE_PORT') ||
+    3000;
+  await app.listen(port);
+
+  console.log('='.repeat(100));
+
+  logAppBootstrap(app);
+
+  console.log('='.repeat(100));
 }
 bootstrap();
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { main } from './common/utils/main.util';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+  beforeAll(async () => {
+    app = await main(AppModule);
   });
 
-  it('/ (GET)', () => {
+  it('GET /', async () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api')
       .expect(200)
       .expect('Hello World!');
   });
